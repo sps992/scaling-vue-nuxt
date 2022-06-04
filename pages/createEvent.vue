13adcf88ed9fce3e286603511a39c06af1bcc20c
sam <support@sps-creativedesig.co.uk>
2022-06-04 15:37:04 +0100

    Adding layouts dir and createEvent template

--- pages/createEvent.vue
+++ pages/createEvent.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="">
-    <h1 class="">Events</h1>
+    <h1 class="">Create Events</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+
 }
 </script>
